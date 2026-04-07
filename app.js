454153c21c69d527a1dcac9c8333084aab9bd178
vilnis <vjatnieks@loyola.edu>
2026-04-06 22:42:52 -0400

    added comments

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
-const gameCont = require('./controller/GameController');
+const gameController = require('./controller/GameController');
 
 const app = express();
 
-app.use(morgan('dev'));
-app.use(express.static('view'));
+app.use(morgan('dev')); //For better logging, we use morgan
+app.use(express.static('view')); // Static page server will use the folder 'view'
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/games', gameCont.getAll);
-app.get('/game/:game_id', gameCont.get);
-app.post('/game', gameCont.postCreateUpdate);
-app.get('/deletegame/:game_id', gameCont.getDelete);
+app.get('/games', gameController.getAll);
+app.get('/game/:game_id', gameController.get);
+app.post('/game', gameController.postCreateUpdate);
+app.get('/deletegame/:game_id', gameController.getDelete);
 
 module.exports = app;
